ff0c041641704d62a7005fb960220392004dafe3
tymino <d_team-lex@bk.ru>
2022-01-22 17:04:39 +0300

    fixed all

--- src/server.js
+++ src/server.js
@@ -1,9 +1,34 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import { Pool } from 'pg';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("hello");
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-app.listen(3000, () => console.log('Server start'));
+app.get('/api/table-component', async (req, res) => {
+  try {
+    const resTableData = await pool.query('SELECT * FROM test_table_component');
+    const resTableName = await pool.query('SELECT * FROM test_table_component_name_ru');
+
+    const tableObj = { header: resTableName.rows, body: resTableData.rows };
+
+    res.json(tableObj);
+  } catch (error) {
+    throw error;
+  }
+});
+
+app.listen(PORT, () => console.log('Server start'));
